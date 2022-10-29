98c190dde33e042f547a1a728dba7cb249ac2b01
Takashi Fukuyama <osakalivetheater@gmail.com>
2022-10-29 20:18:17 +0900

    大丈夫

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'hoiqoo App',
+      title: 'hoiq Ap',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
