fc89064f8be56e5cce7f6de8374c12118d5968c1
saitotaishi <114033536+saitotaishi@users.noreply.github.com>
2022-11-10 11:29:49 +0900

    登録機能の追加

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -1,94 +1,153 @@
 import 'package:flutter/material.dart';
-import './saito.dart';
-import './oden.dart';
-import './takasu.dart';
-import './fukushin.dart';
+import 'package:hoiqoo/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-//　登録用のページ
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
+class MyAuthPage extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'hoiqoo App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'hoiqoo'),
-    );
-  }
+  _MyAuthPageState createState() => _MyAuthPageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAuthPageState extends State<MyAuthPage> {
+  String newUserEmail = "";
+  String newUserPassword = "";
+  String infoText = "";
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.all(40),
-              child: const Text('hoiqoo スキルテスト', style: TextStyle(fontSize: 24)),
-            ),
-            const Nav(page: Saito(label: '齋藤'), label: "齋藤"),
-            const Nav(page: Oden(label: 'おでん'), label: "おでん"),
-            const Nav(page: Takasu(label: '高須'), label: "高須"),
-            const Nav(page: Fukushin(label: 'フクシン'), label: "フクシン"),
-          ],
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: "メールアドレス"),
+                onChanged: (String value) {
+                  setState(() {
+                    newUserEmail = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード（６文字以上）"),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    final UserCredential result =
+                    await auth.createUserWithEmailAndPassword(
+                      email: newUserEmail,
+                      password: newUserPassword,
+                    );
+
+                    await Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context){
+                        return MyApp();
+                      }),
+                    );
+                  } catch (e) {
+                    setState(() {
+                      infoText = "登録に失敗しました：${e.toString()}";
+                    });
+                  }
+                },
+                child: Text("登録する"),
+              ),
+              const SizedBox(height: 8),
+              Text(infoText)
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class Nav extends StatelessWidget {
-  const Nav({
-    Key? key, required this.page, required this.label,
-  }) : super(key: key);
 
-  final Widget page;
-  final String label;
+
+
+
+
+
+
+/*
+class MyAuthPage extends StatefulWidget{
+  @override
+  _MyAuthPageState createState() => _MyAuthPageState();
+}
+
+class _MyAuthPageState extends State<MyAuthPage> {
+  String newUserEmail = "";
+  String newUserPassword = "";
+  String infoText = "";
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context){
-          return page;
-        }));
-      },
-      child: Container(
-        margin: const EdgeInsets.all(20),
-        width: MediaQuery.of(context).size.width * 0.8,
-        height: MediaQuery.of(context).size.width* 0.15,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.grey,
-            width: 1.0,
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: "メールアドレス"),
+                onChanged: (String value) {
+                  setState(() {
+                    newUserEmail = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード（６文字以上）"),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    final UserCredential result =
+                    await auth.createUserWithEmailAndPassword(
+                      email: newUserEmail,
+                      password: newUserPassword,
+                    );
+
+                    await Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context){
+                        return MyApp();
+                      }),
+                    );
+                  } catch (e) {
+                    setState(() {
+                      infoText = "登録に失敗しました：${e.toString()}";
+                    });
+                  }
+                },
+                child: Text("登録する"),
+              ),
+              const SizedBox(height: 8),
+              Text(infoText)
+            ],
           ),
-          borderRadius: BorderRadius.circular(10),
         ),
-        child: Text(label, style: const TextStyle(fontSize: 16)),
       ),
     );
   }
-}
+}*/
