ca62919e76cd3d14997d943eaaffd92f2c408a26
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-11-29 20:20:09 +0900

    freezedを使用

--- lib/class/ui/offer_ui.dart
+++ lib/class/ui/offer_ui.dart
@@ -1,22 +1,17 @@
-// TODO freezed導入したい
-class OfferUi {
-  OfferUi({
-    required this.offerId,
-    required this.enterpriseName,
-    required this.startAt,
-    required this.endAt,
-    required this.address,
-    required this.remuneration,
-    required this.tags,
-    required this.profileImageUrl,
-  });
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  int offerId;
-  String enterpriseName;
-  DateTime startAt;
-  DateTime endAt;
-  String address;
-  String remuneration;
-  List<String> tags;
-  String profileImageUrl;
+part 'offer_ui.freezed.dart';
+
+@freezed
+class OfferUi with _$OfferUi {
+  const factory OfferUi({
+    required int offerId,
+    required String enterpriseName,
+    required DateTime startAt,
+    required DateTime endAt,
+    required String address,
+    required int remuneration,
+    required List<String> tags,
+    required String profileImageUrl,
+  }) = _OfferUi;
 }
---
+++ lib/class/ui/offer_ui.freezed.dart
@@ -0,0 +1,290 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'offer_ui.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$OfferUi {
+  int get offerId => throw _privateConstructorUsedError;
+  String get enterpriseName => throw _privateConstructorUsedError;
+  DateTime get startAt => throw _privateConstructorUsedError;
+  DateTime get endAt => throw _privateConstructorUsedError;
+  String get address => throw _privateConstructorUsedError;
+  int get remuneration => throw _privateConstructorUsedError;
+  List<String> get tags => throw _privateConstructorUsedError;
+  String get profileImageUrl => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $OfferUiCopyWith<OfferUi> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $OfferUiCopyWith<$Res> {
+  factory $OfferUiCopyWith(OfferUi value, $Res Function(OfferUi) then) =
+      _$OfferUiCopyWithImpl<$Res, OfferUi>;
+  @useResult
+  $Res call(
+      {int offerId,
+      String enterpriseName,
+      DateTime startAt,
+      DateTime endAt,
+      String address,
+      int remuneration,
+      List<String> tags,
+      String profileImageUrl});
+}
+
+/// @nodoc
+class _$OfferUiCopyWithImpl<$Res, $Val extends OfferUi>
+    implements $OfferUiCopyWith<$Res> {
+  _$OfferUiCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? offerId = null,
+    Object? enterpriseName = null,
+    Object? startAt = null,
+    Object? endAt = null,
+    Object? address = null,
+    Object? remuneration = null,
+    Object? tags = null,
+    Object? profileImageUrl = null,
+  }) {
+    return _then(_value.copyWith(
+      offerId: null == offerId
+          ? _value.offerId
+          : offerId // ignore: cast_nullable_to_non_nullable
+              as int,
+      enterpriseName: null == enterpriseName
+          ? _value.enterpriseName
+          : enterpriseName // ignore: cast_nullable_to_non_nullable
+              as String,
+      startAt: null == startAt
+          ? _value.startAt
+          : startAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      endAt: null == endAt
+          ? _value.endAt
+          : endAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      address: null == address
+          ? _value.address
+          : address // ignore: cast_nullable_to_non_nullable
+              as String,
+      remuneration: null == remuneration
+          ? _value.remuneration
+          : remuneration // ignore: cast_nullable_to_non_nullable
+              as int,
+      tags: null == tags
+          ? _value.tags
+          : tags // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      profileImageUrl: null == profileImageUrl
+          ? _value.profileImageUrl
+          : profileImageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_OfferUiCopyWith<$Res> implements $OfferUiCopyWith<$Res> {
+  factory _$$_OfferUiCopyWith(
+          _$_OfferUi value, $Res Function(_$_OfferUi) then) =
+      __$$_OfferUiCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int offerId,
+      String enterpriseName,
+      DateTime startAt,
+      DateTime endAt,
+      String address,
+      int remuneration,
+      List<String> tags,
+      String profileImageUrl});
+}
+
+/// @nodoc
+class __$$_OfferUiCopyWithImpl<$Res>
+    extends _$OfferUiCopyWithImpl<$Res, _$_OfferUi>
+    implements _$$_OfferUiCopyWith<$Res> {
+  __$$_OfferUiCopyWithImpl(_$_OfferUi _value, $Res Function(_$_OfferUi) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? offerId = null,
+    Object? enterpriseName = null,
+    Object? startAt = null,
+    Object? endAt = null,
+    Object? address = null,
+    Object? remuneration = null,
+    Object? tags = null,
+    Object? profileImageUrl = null,
+  }) {
+    return _then(_$_OfferUi(
+      offerId: null == offerId
+          ? _value.offerId
+          : offerId // ignore: cast_nullable_to_non_nullable
+              as int,
+      enterpriseName: null == enterpriseName
+          ? _value.enterpriseName
+          : enterpriseName // ignore: cast_nullable_to_non_nullable
+              as String,
+      startAt: null == startAt
+          ? _value.startAt
+          : startAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      endAt: null == endAt
+          ? _value.endAt
+          : endAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      address: null == address
+          ? _value.address
+          : address // ignore: cast_nullable_to_non_nullable
+              as String,
+      remuneration: null == remuneration
+          ? _value.remuneration
+          : remuneration // ignore: cast_nullable_to_non_nullable
+              as int,
+      tags: null == tags
+          ? _value._tags
+          : tags // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      profileImageUrl: null == profileImageUrl
+          ? _value.profileImageUrl
+          : profileImageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_OfferUi implements _OfferUi {
+  const _$_OfferUi(
+      {required this.offerId,
+      required this.enterpriseName,
+      required this.startAt,
+      required this.endAt,
+      required this.address,
+      required this.remuneration,
+      required final List<String> tags,
+      required this.profileImageUrl})
+      : _tags = tags;
+
+  @override
+  final int offerId;
+  @override
+  final String enterpriseName;
+  @override
+  final DateTime startAt;
+  @override
+  final DateTime endAt;
+  @override
+  final String address;
+  @override
+  final int remuneration;
+  final List<String> _tags;
+  @override
+  List<String> get tags {
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_tags);
+  }
+
+  @override
+  final String profileImageUrl;
+
+  @override
+  String toString() {
+    return 'OfferUi(offerId: $offerId, enterpriseName: $enterpriseName, startAt: $startAt, endAt: $endAt, address: $address, remuneration: $remuneration, tags: $tags, profileImageUrl: $profileImageUrl)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_OfferUi &&
+            (identical(other.offerId, offerId) || other.offerId == offerId) &&
+            (identical(other.enterpriseName, enterpriseName) ||
+                other.enterpriseName == enterpriseName) &&
+            (identical(other.startAt, startAt) || other.startAt == startAt) &&
+            (identical(other.endAt, endAt) || other.endAt == endAt) &&
+            (identical(other.address, address) || other.address == address) &&
+            (identical(other.remuneration, remuneration) ||
+                other.remuneration == remuneration) &&
+            const DeepCollectionEquality().equals(other._tags, _tags) &&
+            (identical(other.profileImageUrl, profileImageUrl) ||
+                other.profileImageUrl == profileImageUrl));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      offerId,
+      enterpriseName,
+      startAt,
+      endAt,
+      address,
+      remuneration,
+      const DeepCollectionEquality().hash(_tags),
+      profileImageUrl);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_OfferUiCopyWith<_$_OfferUi> get copyWith =>
+      __$$_OfferUiCopyWithImpl<_$_OfferUi>(this, _$identity);
+}
+
+abstract class _OfferUi implements OfferUi {
+  const factory _OfferUi(
+      {required final int offerId,
+      required final String enterpriseName,
+      required final DateTime startAt,
+      required final DateTime endAt,
+      required final String address,
+      required final int remuneration,
+      required final List<String> tags,
+      required final String profileImageUrl}) = _$_OfferUi;
+
+  @override
+  int get offerId;
+  @override
+  String get enterpriseName;
+  @override
+  DateTime get startAt;
+  @override
+  DateTime get endAt;
+  @override
+  String get address;
+  @override
+  int get remuneration;
+  @override
+  List<String> get tags;
+  @override
+  String get profileImageUrl;
+  @override
+  @JsonKey(ignore: true)
+  _$$_OfferUiCopyWith<_$_OfferUi> get copyWith =>
+      throw _privateConstructorUsedError;
+}
