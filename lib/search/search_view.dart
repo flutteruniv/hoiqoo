6849b7028a3c0b3121c5b7f76863232dc72b18a3
Tetsuo Oda <78355880+salan70@users.noreply.github.com>
2022-12-15 13:44:47 +0900

    検索画面のUIを作成 (#14)
    * search画面の大まかな画面レイアウトを作成
    
    * ファイル名、ディレクトリ構成を変更
    
    * 画面下の余白がスクロール時にのみ表示されるよう修正
    
    * Offerクラスを作成
    
    * Enterpriseクラスを作成
    
    * offerIdの型をintに修正
    
    * ファイル名の頭文字を小文字に修正
    
    * OfferMockDataを作成
    
    * OfferMockDataを作成
    
    * OfferUiクラスを作成
    
    * profileImageの型を修正
    
    * profileImageをprofileImageUrlに変更
    
    * freezedを導入
    
    * profileImageを修正
    
    * freezedを使用
    
    * offerUiMockDataを作成
    
    * パラメータを修正
    
    * パラメータを修正
    
    * OfferUiのパラメータ変更に伴い、モックデータを修正
    
    * モックデータを表示するよう変更
    
    * ファイルと空のクラスを作成
    
    * paddingで幅を指定するよう変更
    
    * 表示するモックデータの数を追加
    
    * FABの背景色のopacityを設定
    
    * レイアウトを微調整
    
    * タグに「#」を追加
    
    * pubspec.lockを追加
    
    * 不要になったファイルを削除
    
    * 求人ごとのCardを独自クラスとして切り出し

---
+++ lib/search/search_view.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:hoiqoo/class/ui/offer_ui.dart';
+import 'package:hoiqoo/mock_data/offer_ui_mock_data.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<OfferUi> offerUiList = [
+      OfferUiMockData.data1,
+      OfferUiMockData.data2,
+      OfferUiMockData.data3,
+      OfferUiMockData.data1,
+      OfferUiMockData.data2,
+      OfferUiMockData.data3,
+      OfferUiMockData.data1,
+      OfferUiMockData.data2,
+      OfferUiMockData.data3,
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('募集をさがす'),
+        centerTitle: true,
+        elevation: 0.5,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListView(
+          children: [
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+                childAspectRatio: 4 / 5,
+              ),
+              itemCount: offerUiList.length,
+              itemBuilder: (context, index) {
+                final offerUi = offerUiList[index];
+                return OfferCard(offerUi: offerUi);
+              },
+            ),
+            const SizedBox(
+              height: 64,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: const Color(0xFFF08784).withOpacity(0.9),
+        icon: const Icon(Icons.search_rounded),
+        label: const Text('日付・条件を選択する'),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class OfferCard extends StatelessWidget {
+  const OfferCard({
+    Key? key,
+    required this.offerUi,
+  }) : super(key: key);
+
+  final OfferUi offerUi;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(16),
+              topRight: Radius.circular(16),
+            ),
+            child: SizedBox(
+              height: 120,
+              width: double.infinity,
+              child: Image.network(
+                offerUi.profileImageUrl,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(
+            offerUi.enterpriseName,
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          Text(
+            offerUi.workingHours,
+          ),
+          Text(
+            offerUi.address,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          Text(
+            offerUi.remuneration,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (final tag in offerUi.tags)
+                Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: Text(
+                    tag,
+                    style: const TextStyle(
+                      color: Color(0xFFF08784),
+                    ),
+                  ),
+                ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
